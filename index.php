ca02a3401652724921821de7d5f6054245f05ec3
admin <admin@iMac3.local>
2017-10-27 16:54:37 +0200

    Modification du index.html vers index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    require_once("./template/head.html");
+    require_once("./template/header.html");
+    require_once("./template/content.html");
+    require_once("./template/footer.html");
+    require_once("./template/foot.html");
+?>
